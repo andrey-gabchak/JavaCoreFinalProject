e1c9255318a3164a2f3106288b69fa58ff9d2720
andrey.gabchak@gmail.com <andrey.gabchak@gmail.com>
2016-04-11 16:40:15 +0300

    Search of factorial of entered number

--- src/main/java/Factorial.java
+++ src/main/java/Factorial.java
@@ -2,8 +2,21 @@ import java.util.ArrayList;
 
 /**
  * Created by coura on 10.04.2016.
+ *
+ * Search of factorial of entered number.
  */
+
 public class Factorial {
 
+    public int searchOfFactorial(int number){
+
+        int facrorial = 1;
+
+        for (int i = 1; i <= number; i++) {
+            facrorial *= i;
+        }
+
+        return facrorial;
+    }
 
 }
