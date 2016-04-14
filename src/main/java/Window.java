8248a1c07a9e97f1e0c838ddb3fa9a5817c2e5fb
andrey.gabchak@gmail.com <andrey.gabchak@gmail.com>
2016-04-14 23:39:44 +0300

    Added output method

--- src/main/java/Window.java
+++ src/main/java/Window.java
@@ -2,9 +2,7 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.lang.reflect.Array;
 import java.util.ArrayList;
-import java.util.Arrays;
 
 /**
  * Created by coura on 11.04.2016.
@@ -13,13 +11,15 @@ import java.util.Arrays;
  */
 public class Window extends JFrame {
 
-    private JCheckBox numberSeriesFibinacciCheckBox;
+    private JCheckBox numericalSeriesFibonacciCheckBox;
     private JCheckBox maxNumberOfFibonacciCheckBox;
     private JCheckBox factorialCheckBox;
     private JLabel descriptionField;
     private JButton findButton;
-    private JTextField enteredNumerTextField;
-    private JLabel resultLabel;
+    private JTextField inputNumberTextField;
+    private JLabel resultNumericalSeriesFibonacciLabel;
+    private JLabel resultMaxNumberFibonacciFibLabel;
+    private JLabel resultFactorialLabel;
 
     public Window() {
 
@@ -29,17 +29,19 @@ public class Window extends JFrame {
         setTitle("Фибоначчи и факториал");
 
         descriptionField = new JLabel("Программа вычисления следующих параметров для введенного числа.");
-        numberSeriesFibinacciCheckBox = new JCheckBox("Рассчитать ряд Фибоначчи к введенному числу");
+        numericalSeriesFibonacciCheckBox = new JCheckBox("Рассчитать ряд Фибоначчи к введенному числу");
         maxNumberOfFibonacciCheckBox = new JCheckBox("Рассчитать число Фибоначчи максимально приближенно у введенному");
         factorialCheckBox = new JCheckBox("Найти факториал числа");
-        enteredNumerTextField = new JTextField("Введите число");
+        inputNumberTextField = new JTextField("5");
         findButton = new JButton("Рассчитать!");
-        resultLabel = new JLabel();
+        resultNumericalSeriesFibonacciLabel = new JLabel();
+        resultMaxNumberFibonacciFibLabel = new JLabel();
+        resultFactorialLabel = new JLabel();
 
         add(descriptionField,
                 new GridBagConstraints(0, 0, 1, 1, 0.0, 0.9, GridBagConstraints.NORTH,
-                        GridBagConstraints.HORIZONTAL, new Insets(2, 5, 5, 3), 0, 0));
-        add(numberSeriesFibinacciCheckBox,
+                        GridBagConstraints.HORIZONTAL, new Insets(10, 5, 5, 3), 0, 0));
+        add(numericalSeriesFibonacciCheckBox,
                 new GridBagConstraints(0, 1, 1, 1, 0.0, 0.9, GridBagConstraints.NORTH,
                         GridBagConstraints.HORIZONTAL, new Insets(5, 5, 5, 3), 0, 0));
         add(maxNumberOfFibonacciCheckBox,
@@ -48,15 +50,21 @@ public class Window extends JFrame {
         add(factorialCheckBox,
                 new GridBagConstraints(0, 3, 1, 1, 0.0, 0.9, GridBagConstraints.NORTH,
                         GridBagConstraints.HORIZONTAL, new Insets(2, 5, 5, 3), 0, 0));
-        add(enteredNumerTextField,
+        add(inputNumberTextField,
                 new GridBagConstraints(0, 4, 1, 1, 0.0, 0.9, GridBagConstraints.NORTH,
                         GridBagConstraints.HORIZONTAL, new Insets(7, 5, 5, 3), 0, 0));
         add(findButton,
                 new GridBagConstraints(0, 5, 1, 1, 0.0, 0.9, GridBagConstraints.CENTER,
                         GridBagConstraints.HORIZONTAL, new Insets(12, 5, 5, 3), 0, 0));
-        add(resultLabel,
+        add(resultNumericalSeriesFibonacciLabel,
                 new GridBagConstraints(0, 6, 1, 1, 0.0, 0.9, GridBagConstraints.CENTER,
                         GridBagConstraints.HORIZONTAL, new Insets(12, 5, 5, 3), 0, 0));
+        add(resultMaxNumberFibonacciFibLabel,
+                new GridBagConstraints(0, 7, 1, 1, 0.0, 0.9, GridBagConstraints.CENTER,
+                        GridBagConstraints.HORIZONTAL, new Insets(2, 5, 5, 3), 0, 0));
+        add(resultFactorialLabel,
+                new GridBagConstraints(0, 8, 1, 1, 0.0, 0.9, GridBagConstraints.CENTER,
+                        GridBagConstraints.HORIZONTAL, new Insets(2, 5, 5, 3), 0, 0));
 
         findButton.addActionListener(new findButtonActionListener());
 
@@ -68,16 +76,52 @@ public class Window extends JFrame {
 
         @Override
         public void actionPerformed(ActionEvent e) {
-            int number = Integer.parseInt(enteredNumerTextField.getText());
+            int number = Integer.parseInt(inputNumberTextField.getText());
+            outputResult(number);
+        }
+
+        private void outputResult(int number) {
             Fibonacci fibonacci = new Fibonacci();
-            if (numberSeriesFibinacciCheckBox.isSelected()) {
+
+            if (numericalSeriesFibonacciCheckBox.isSelected()) {
                 ArrayList<Integer> fibonacciNumericalSeries = fibonacci.numericalSeriesFibonacci(number);
-                resultLabel.setText(fibonacci.fibNumSeriesToString(fibonacciNumericalSeries));
+                resultNumericalSeriesFibonacciLabel.setText("Ряд Фибоначчи: "
+                        + fibonacci.fibNumSeriesToString(fibonacciNumericalSeries));
+                resultMaxNumberFibonacciFibLabel.setVisible(false);
+                resultFactorialLabel.setVisible(false);
+                resultNumericalSeriesFibonacciLabel.setVisible(true);
+
+                if (maxNumberOfFibonacciCheckBox.isSelected()) {
+                    resultMaxNumberFibonacciFibLabel.setText("Максимальное число Фибоначчи: "
+                            + String.valueOf(fibonacci.maxNumberFibonacci(number)));
+                    resultMaxNumberFibonacciFibLabel.setVisible(true);
+
+                    if (factorialCheckBox.isSelected()) {
+                        Factorial factorial = new Factorial();
+                        resultFactorialLabel.setText("Факториал: "
+                                + String.valueOf(factorial.searchOfFactorial(number)));
+                        resultFactorialLabel.setVisible(true);
+                    }
+                }
             } else if (maxNumberOfFibonacciCheckBox.isSelected()) {
-                resultLabel.setText(String.valueOf(fibonacci.maxNumberFibonacci(number)));
+                resultMaxNumberFibonacciFibLabel.setText("Максимальное число Фибоначчи: "
+                        + String.valueOf(fibonacci.maxNumberFibonacci(number)));
+                resultNumericalSeriesFibonacciLabel.setVisible(false);
+                resultFactorialLabel.setVisible(false);
+                resultMaxNumberFibonacciFibLabel.setVisible(true);
+
+                if (factorialCheckBox.isSelected()) {
+                    Factorial factorial = new Factorial();
+                    resultFactorialLabel.setText("Факториал: "
+                            + String.valueOf(factorial.searchOfFactorial(number)));
+                    resultFactorialLabel.setVisible(true);
+                }
             } else if (factorialCheckBox.isSelected()) {
                 Factorial factorial = new Factorial();
-                resultLabel.setText(String.valueOf(factorial.searchOfFactorial(number)));
+                resultFactorialLabel.setText("Факториал: "
+                        + String.valueOf(factorial.searchOfFactorial(number)));
+                resultNumericalSeriesFibonacciLabel.setVisible(false);
+                resultMaxNumberFibonacciFibLabel.setVisible(false);
             }
         }
     }
