94db65e0f76a5cd3a7950d81964482e9f1db66ef
andrey.gabchak@gmail.com <andrey.gabchak@gmail.com>
2016-04-15 19:58:09 +0300

    Remove the two latest designer character in a series of Fibonacci

--- src/main/java/Fibonacci.java
+++ src/main/java/Fibonacci.java
@@ -2,35 +2,36 @@ import java.util.ArrayList;
 
 /**
  * Created by coura on 10.04.2016.
- *
+ * <p>
  * Search of numerical series and max number Fibonacci.
  */
 
 public class Fibonacci {
 
-    public ArrayList<Integer> numericalSeriesFibonacci(int maxNumber){
+    public ArrayList<Integer> numericalSeriesFibonacci(int maxNumber) {
 
         if (maxNumber == 0) return null;
 
         ArrayList<Integer> numSeriesFibonacci = new ArrayList<Integer>();
 
-        if (maxNumber == 1){
+        if (maxNumber == 1) {
             numSeriesFibonacci.add(1);
             return numSeriesFibonacci;
         }
 
         for (int i = 0; i < maxNumber; i++) {
             if (i < 2) numSeriesFibonacci.add(i, 1);
-            if ((i >= 2) && ((numSeriesFibonacci.get(i - 1) + numSeriesFibonacci.get(i - 2)) <= maxNumber)){
+            if ((i >= 2) && ((numSeriesFibonacci.get(i - 1) + numSeriesFibonacci.get(i - 2)) <= maxNumber)) {
                 numSeriesFibonacci.add(i, numSeriesFibonacci.get(i - 1) + numSeriesFibonacci.get(i - 2));
             }
-            if ((i >= 2) && ((numSeriesFibonacci.get(i - 1) + numSeriesFibonacci.get(i - 2)) > maxNumber)){
+            if ((i >= 2) && ((numSeriesFibonacci.get(i - 1) + numSeriesFibonacci.get(i - 2)) > maxNumber)) {
                 break;
             }
         }
         return numSeriesFibonacci;
     }
-    public int maxNumberFibonacci(int maxNumber){
+
+    public int maxNumberFibonacci(int maxNumber) {
 
         if (maxNumber == 0) return 0;
         if (maxNumber == 1) return 1;
@@ -39,7 +40,7 @@ public class Fibonacci {
         int maxFibonacci = 0;
 
         for (int i = 1; i < maxNumber; i++) {
-            if ((maxFibonacci + early) <= maxNumber){
+            if ((maxFibonacci + early) <= maxNumber) {
                 maxFibonacci = previos + early;
                 early = previos;
                 previos = maxFibonacci;
@@ -49,13 +50,15 @@ public class Fibonacci {
         return maxFibonacci;
     }
 
-    public String fibNumSeriesToString(ArrayList fibonacciNS){
+    public String fibNumSeriesToString(ArrayList fibonacciNS) {
         StringBuilder fib = new StringBuilder();
 
         for (Object element : fibonacciNS) {
             fib.append(element);
             fib.append(", ");
         }
+        fib.deleteCharAt(fib.length() - 1);
+        fib.deleteCharAt(fib.length() - 1);
 
         String result = String.valueOf(fib);
         return result;
